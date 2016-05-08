bfbde351bd7b28775b95a83c306ceb09196bf37c
Razzwan <razvanlomov@gmail.com>
2016-05-08 17:15:46 +0300

    Добился максимальной лаконичности и красоты

--- vendor/liw/core/ErrorHandler.php
+++ vendor/liw/core/ErrorHandler.php
@@ -13,21 +13,21 @@ class ErrorHandler
      */
     static public function getErrorName($error){
         $errors = [
-            E_ERROR => 'ERROR',
-            E_WARNING => 'WARNING',
-            E_PARSE => 'PARSE',
-            E_NOTICE => 'NOTICE',
-            E_CORE_ERROR => 'CORE_ERROR',
-            E_CORE_WARNING => 'CORE_WARNING',
-            E_COMPILE_ERROR => 'COMPILE_ERROR',
-            E_COMPILE_WARNING => 'COMPILE_WARNING',
-            E_USER_ERROR => 'USER_ERROR',
-            E_USER_WARNING => 'USER_WARNING',
-            E_USER_NOTICE => 'USER_NOTICE',
-            E_STRICT => 'STRICT',
+            E_ERROR             => 'ERROR',
+            E_WARNING           => 'WARNING',
+            E_PARSE             => 'PARSE',
+            E_NOTICE            => 'NOTICE',
+            E_CORE_ERROR        => 'CORE_ERROR',
+            E_CORE_WARNING      => 'CORE_WARNING',
+            E_COMPILE_ERROR     => 'COMPILE_ERROR',
+            E_COMPILE_WARNING   => 'COMPILE_WARNING',
+            E_USER_ERROR        => 'USER_ERROR',
+            E_USER_WARNING      => 'USER_WARNING',
+            E_USER_NOTICE       => 'USER_NOTICE',
+            E_STRICT            => 'STRICT',
             E_RECOVERABLE_ERROR => 'RECOVERABLE_ERROR',
-            E_DEPRECATED => 'DEPRECATED',
-            E_USER_DEPRECATED => 'USER_DEPRECATED',
+            E_DEPRECATED        => 'DEPRECATED',
+            E_USER_DEPRECATED   => 'USER_DEPRECATED',
         ];
         if(array_key_exists($error, $errors)){
             return $errors[$error] . " [$error]";
